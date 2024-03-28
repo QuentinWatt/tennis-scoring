333277beed9c4a6d9c0746eb46770dec564e4ae8
Quentin Watt <quentin@qcasts.com>
2024-03-28 13:39:40 +0200

    Create tests

---
+++ classes/Game.spec.ts
@@ -0,0 +1,51 @@
+import { beforeEach, describe, expect, it } from "vitest";
+import { Game } from "./Game";
+
+describe("It passes the Game Class test", () => {
+  let game: Game;
+  // Setup a new game for each test
+  beforeEach(() => {
+    game = new Game();
+  });
+
+  it("Has default values for the scoreboard", () => {
+    expect(game.score1).toEqual(0);
+    expect(game.score2).toEqual(0);
+    expect(game.scoreboard()).toEqual("Love all");
+  });
+
+  it("Can increase the player 1 score", () => {
+    expect(game.scoreboard()).toEqual("Love all");
+    game.player1Point();
+    expect(game.scoreboard()).toEqual("15 - Love");
+    game.player1Point();
+    expect(game.scoreboard()).toEqual("30 - Love");
+    game.player1Point();
+    expect(game.scoreboard()).toEqual("40 - Love");
+  });
+
+  it("Can increase the player 2 score", () => {
+    expect(game.scoreboard()).toEqual("Love all");
+    game.player2Point();
+    expect(game.scoreboard()).toEqual("Love - 15");
+    game.player2Point();
+    expect(game.scoreboard()).toEqual("Love - 30");
+    game.player2Point();
+    expect(game.scoreboard()).toEqual("Love - 40");
+  });
+
+  it("Can suffix 'all', or Duece to equal scores", () => {
+    expect(game.scoreboard()).toEqual("Love all");
+    game.player1Point();
+    game.player2Point();
+    expect(game.scoreboard()).toEqual("15 all");
+
+    game.player1Point();
+    game.player2Point();
+    expect(game.scoreboard()).toEqual("30 all");
+
+    game.player1Point();
+    game.player2Point();
+    expect(game.scoreboard()).toEqual("Deuce");
+  });
+});
